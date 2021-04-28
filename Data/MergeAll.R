e2fa7b25179fdc40d40c43b78d20d0846d9779ec
FWyckmans <fwyckmans@gmail.com>
2021-04-28 17:27:41 +0200

    Finished merging all

--- Data/MergeAll.R
+++ Data/MergeAll.R
@@ -6,3 +6,21 @@ Datapath = "Data/Pavlovia/ProcessedData/"
 Output_path = "Data/ProcessedData/"
 
 dQ <- read.delim("Data/LimeSurveyQuestionnaires/ProcessedData/dQuestionnaireTot.txt")
+dPav <- read.delim(paste0(Datapath, "dPav.txt"))
+
+dQ <- AddDummyCol(dQ, colnames(dPav[2:length(dPav)]))
+
+for (i in dQ$Mail1) {
+  if (i %in% dPav$Email){
+    for (n in colnames(dPav[2:length(dPav)])) {
+      dQ[which(dQ$Mail1 == i), n] <- dPav[which(dPav$Email == i), n]
+    }
+  }
+}
+
+dMailTot <- dQ[2]
+dTot <- dQ[, -which(names(dQ) %in% c("Mail1"))]
+
+write.table(dTot, paste0(Output_path, "dTot.txt"), col.names = T, row.names = F, sep = "\t", dec = ".")
+write_xlsx(dTot, paste0(Output_path, "dTot.xlsx"))
+write.table(dMailTot, "AdditionalInfo/MailList/dMailTot.txt", col.names = T, row.names = F, sep = "\t", dec = ".")
