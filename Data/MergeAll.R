83fa1156c84d6d5804895fc59d4a2f34ceff1248
FWyckmans <fwyckmans@gmail.com>
2021-04-28 17:08:23 +0200

    Started script to merge all dF into a final one

---
+++ Data/MergeAll.R
@@ -0,0 +1,8 @@
+##### Pavlovia Day 1
+# Initialization
+source("VGS_Init.R")
+
+Datapath = "Data/Pavlovia/ProcessedData/"
+Output_path = "Data/ProcessedData/"
+
+dQ <- read.delim("Data/LimeSurveyQuestionnaires/ProcessedData/dQuestionnaireTot.txt")
