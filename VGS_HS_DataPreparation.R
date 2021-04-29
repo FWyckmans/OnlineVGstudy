e082b730852b03271d6483b780489bd67338558e
FWyckmans <fwyckmans@gmail.com>
2021-04-29 11:31:48 +0200

    Created the hyperscript for datacreation

---
+++ VGS_HS_DataPreparation.R
@@ -0,0 +1,10 @@
+##### Data Preparation HyperScript
+source("VGS_Init.R")
+
+source("Data/LimeSurveyQuestionnaires/Script/ImportLS1.R")
+source("Data/LimeSurveyQuestionnaires/Script/ImportLS2.R")
+source("Data/LimeSurveyQuestionnaires/Script/MergeLSdf.R")
+
+source("Data/Pavlovia/Script/ImportPavlovia.R")
+
+source("Data/MergeAll.R")
