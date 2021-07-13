73095efa2b9fa1049dc0849e71306e5ccc257a56
FWyckmans <fwyckmans@gmail.com>
2021-07-13 12:03:46 +0200

    Added the GnG to HS

--- VGS_HS_DataPreparation.R
+++ VGS_HS_DataPreparation.R
@@ -6,6 +6,7 @@ source("Data/LimeSurveyQuestionnaires/Script/ImportLS2.R")
 source("Data/LimeSurveyQuestionnaires/Script/MergeLSdf.R")
 
 source("Data/Pavlovia/Script/ImportPavlovia.R")
+source("Data/Pavlovia/Script/ImportGnG.R")
 
 source("Data/MergeAll.R")
 
